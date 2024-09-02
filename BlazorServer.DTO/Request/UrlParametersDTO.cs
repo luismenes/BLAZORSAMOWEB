f6a3cc78f4dd0f8bd9f2e12ea8741290171dc9ab
luismenes <luismenes027@gmail.com>
2024-09-02 18:45:19 -0500

    InicialCOntratacion

---
+++ BlazorServer.DTO/Request/UrlParametersDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorServer.DTO.Request
+{
+    public class UrlParametersDTO
+    {
+        public long UserId { get; set; }
+        public string KeySession { get; set; }
+        public List<Parameter> Parameters { get; set; }
+        public class Parameter
+        {
+            public string KeySede { get; set; }
+            public string ValueCliente { get; set; }
+        }
+    }
+}
---
+++ BlazorServer.Presentation/Pages/Contratacion/Contratacion.razor.cs
@@ -0,0 +1,35 @@
+﻿using BlazorServer.DTO.Request;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorServer.Presentation.Pages.Contratacion
+{
+    public partial class Contratacion
+    {
+        [Parameter]
+        public string? param { get; set; }
+
+
+        private UrlParametersDTO urlParametersDTO { get; set; } = new UrlParametersDTO();
+        private bool? isAuthorized = null;
+
+        private void BtnNuevoClick()
+        {
+            // Acción para el botón Nuevo
+        }
+
+        private void BtnGuardarClick()
+        {
+            // Acción para el botón Guardar
+        }
+
+        private void BtnAnularClick()
+        {
+            // Acción para el botón Anular
+        }
+
+        private void BtnBuscarClick()
+        {
+            // Acción para el botón Buscar
+        }
+    }
+}
